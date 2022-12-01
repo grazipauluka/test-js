d80b6aee3ae84d41d1daaf7652822be66aa13173
graziela.pauluka <graziela.pauluka@adevinta.com>
2022-12-01 10:28:39 +0100

    add queryString

--- src/lib/queryString.spec.js
+++ src/lib/queryString.spec.js
@@ -1,12 +1,21 @@
-//const {queryString} = require('./queryString.js')
+const { queryString } = require('./queryString.js');
 
-//describe('Object to query string', () => {
-//    it('should create a valid query string when an object is called')
-//      const obj = {
-//          name: 'Graziela',
-//           profession: 'Trainee'
-//      };
-//       expect(queryString(obj)).toBe(
-//        'name=Graziela&profession=Trainee'
-//     );
-//});
+describe('Object to query string', () => {
+	it('should create a valid query string when an object is called', () => {
+		const obj = {
+			name: 'Graziela',
+			profession: 'Trainee',
+		};
+
+		expect(queryString(obj)).toBe('name=Graziela&profession=Trainee');
+	});
+
+	it('should create a valid query string even when an array is passed with a comma', () => {
+		const obj = {
+			name: 'Graziela',
+			abilities: ['Python', 'Angular'],
+		};
+
+		expect(queryString(obj)).toBe('name=Graziela&abilities=Python,Angular');
+	});
+});
